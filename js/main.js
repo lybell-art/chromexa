8f198f12e9f6074ced2a8f2fd69d8aef0ae16cf2
steliviere <32293736+steliviere@users.noreply.github.com>
2017-12-13 21:16:33 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,55 +18,56 @@ var userData;
 function setup()
 {
 	createCanvas(windowWidth,windowHeight);
-  //class definition
-  loading=new LOADING();
-  intro=new INTRO();
-  selection=new SELENTION();
-  manageChara=new MANAGE_CHARA();
-  sandBox=new SANDBOX();
-  ingame=new INGAME();
-  popup=new POPUP();
+	//class definition
+	loading=new LOADING();
+	intro=new INTRO();
+	selection=new SELENTION();
+	manageChara=new MANAGE_CHARA();
+	sandBox=new SANDBOX();
+	ingame=new INGAME();
+	popup=new POPUP();
 	broadcast=new BROADCAST();
 	screenControl=new SCREEN_CONTROL(45*(1.5*maxWid-0.5),45*cos(PI/6)*(2*maxHei+1));
 	//load data
-  loading.dataLoad();
+	loading.dataLoad();
 }
 function draw()
 {
+	broadcast.renew();
 	if(popupNo==0) sceneExecute();
-  else popupExecute();
+	else popupExecute();
 }
 function sceneExecute()
 {
-  switch(sceneNo)
-  {
-    case -1:loading.execute(); break;
-    case 0:intro.execute(); break;
-    case 1:selection.mainSelect(); break;
-    case 2:selection.stageSelect_single(); break;
-    case 3:selection.modeSelect_multi(); break;
-    case 4:selection.stageSelect_multi(); break;
-    case 10:ingame.setup(); break;
-    case 11:ingame.playerTurn(); break;
-    case 12:ingame.playerTurn_motion(); break;
-    case 13:ingame.enemyTurn(); break;
-    case 14:ingame.enemyTurn_motion(); break;
-    case 15:ingame.result(); break;
-    case 20:manageChara.setup(); break;
-    case 21:manageChara.myChara(); break;
-    case 30:sandbox.setup(); break;
-    case 31:sandbox.execute(); break;
-  }
+	switch(sceneNo)
+	{
+		case -1:loading.execute(); break;
+		case 0:intro.execute(); break;
+		case 1:selection.mainSelect(); break;
+		case 2:selection.stageSelect_single(); break;
+		case 3:selection.modeSelect_multi(); break;
+		case 4:selection.stageSelect_multi(); break;
+		case 10:ingame.setup(); break;
+		case 11:ingame.playerTurn(); break;
+		case 12:ingame.playerTurn_motion(); break;
+		case 13:ingame.enemyTurn(); break;
+		case 14:ingame.enemyTurn_motion(); break;
+		case 15:ingame.result(); break;
+		case 20:manageChara.setup(); break;
+		case 21:manageChara.myChara(); break;
+		case 30:sandbox.setup(); break;
+		case 31:sandbox.execute(); break;
+	}
 }
 function popupExecute()
 {
-  switch(popupNo)
-  {
-    case 1:popup.pause(); break;
-    case 2:popup.preferance(); break;
-    case 3:popup.networking(); break;
-    case 99:popup.error(); break;
-  }
+	switch(popupNo)
+	{
+		case 1:popup.pause(); break;
+		case 2:popup.preferance(); break;
+		case 3:popup.networking(); break;
+		case 99:popup.error(); break;
+	}
 }
 function mousePressed()
 {
@@ -78,10 +79,10 @@ function mouseDragged()
 {
 	var deltaX=mouseX-broadcast.dmouseX;
 	var deltaY=mouseY-broadcast.dmouseY;
-	screenControl.move(deltaX,deltaY);
+	if(sceneNo%10==1 || sceneNo==31) screenControl.move(deltaX,deltaY);
 }
 function mouseWheel(event)
 {
 	var newZoom=screenControl.zoom+0.001*event.delta;
-	screenControl.scale(newZoom,mouseX,mouseY);
+	if(sceneNo%10==1 || sceneNo==31) screenControl.scale(newZoom,mouseX,mouseY);
 }
