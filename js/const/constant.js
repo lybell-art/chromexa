2127eaa20f29049253d5b28bd726ea698b06fe8d
steliviere <32293736+steliviere@users.noreply.github.com>
2017-12-13 22:31:50 +0900

    Create constant.js

---
+++ js/const/constant.js
@@ -0,0 +1,10 @@
+//ingame signal constant
+const _BACK=-1;
+const _SETTING=-2;
+const _NOMOVE=0;
+const _MOVEABLE=1;
+const _FILLER=2;
+const _CHARA=3;
+
+//single_multy constant
+const _MULTYPLAY=0;
