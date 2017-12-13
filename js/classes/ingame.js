35fd1d044572f23022e5069cd6cdc2a5f76943e5
steliviere <32293736+steliviere@users.noreply.github.com>
2017-12-13 22:34:36 +0900

    Create ingame.js

---
+++ js/classes/ingame.js
@@ -0,0 +1,39 @@
+/**
+ *
+ * 인게임 클래스
+ * 
+ * @author steliviere
+ * @date 2017.12.13
+ * @version 0.01
+ *
+ */
+function INGAME()
+{
+	this.world=1;
+	this.stage=1;
+	this.mode=1;
+	this.field=null;
+	this.p1=null;
+	this.p2=null;
+}
+INGAME.prototype.setup()
+{
+	/*
+	this.world=stream.StoG1;
+	this.stage=stream.StoG2;
+	var mapData=resourceBox.map[this.world][this.stage].copy();
+	this.field=new FIELD();
+	this.field.makeField(mapData);
+	screenControl.set(this.field.w,this.field.h);
+	this.p1=new PLAYER();
+	if(this.world==_MULTIPLAY) this.p2=new PLAYER();
+	else this.p2=new ENEMY();
+	*/
+	this.field=new FIELD();
+	this.field.makeField();
+	screenControl.set(this.field.w,this.field.h);
+}
+INGAME.prototype.execute();
+{
+	this.field.draw();
+}
