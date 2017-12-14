090971b66eaf29746dfb8ce9f01575babafcc06b
steliviere <32293736+steliviere@users.noreply.github.com>
2017-12-14 16:23:19 +0900

    Create hexagon.js

---
+++ src/classes/hexagon.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * 육각형 셀의 인덱스 no.
+ * 
+ * @var {int} row	garojul no.
+ * @var {int} col	serojul no.
+ *
+ */
+function COORD(i,j)
+{
+	this.row=i;
+	this.col=j;
+}
+
+function hexCell_trans(start, dir, dist)
+{
+	
+}
