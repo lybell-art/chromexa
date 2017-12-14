d59fba649819ea89346049c7d62fa4b6fd2f0466
steliviere <32293736+steliviere@users.noreply.github.com>
2017-12-14 16:16:41 +0900

    Update constant.js

--- src/const/constant.js
+++ src/const/constant.js
@@ -6,5 +6,13 @@ const _MOVEABLE=1;
 const _FILLER=2;
 const _CHARA=3;
 
+//cell direction constant
+const _HEX_UP=0;
+const _HEX_RIGHTUP=1;
+const _HEX_RIGHTDOWN=2;
+const _HEX_DOWN=3;
+const _HEX_LEFTDOWN=4;
+const _HEX_LEFTUP=5;
+
 //single_multy constant
 const _MULTYPLAY=0;
