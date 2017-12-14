435e706e13ec322f6aa405f550093da1ed3748d5
steliviere <32293736+steliviere@users.noreply.github.com>
2017-12-14 16:48:05 +0900

    Update field.js

--- src/classes/field.js
+++ src/classes/field.js
@@ -11,15 +11,15 @@ function FIELD()
 {
   /**
    *
-   * @var {int} row		필드의 행(가로줄)의 수
-   * @var {int} column		필드의 열(세로줄)의 수
+   * @var {int} rows		필드의 행(가로줄)의 수
+   * @var {int} columns		필드의 열(세로줄)의 수
    * @var {float} w		필드의 가로 길이
    * @var {float} h		필드의 세로 길이
    * @var {object|CELL} cells	셀 오브젝트
    *
    */
-	this.row=0;
-	this.column=0;
+	this.rows=0;
+	this.columns=0;
 	this.w=0;
 	this.h=0;
 	this.cells=[];
@@ -36,17 +36,17 @@ FIELD.prototype.makeField=function()
 	/* 미구현
 	var mapData_kind=mapData.kind;
 	var mapData_who=mapData.who;
-	this.row=mapData.row;
-	this.column=mapData.column;
+	this.rows=mapData.row;
+	this.columns=mapData.column;
 	*/
-	this.row=10;
-	this.column=10;
+	this.rows=10;
+	this.columns=10;
 	this.w=45*(1.5*this.column-0.5);
 	this.h=45*cos(PI/6)*(2*this.row+1);
-	for(var i=0;i<this.row;i++)
+	for(var i=0;i<this.rows;i++)
 	{
 		this.cells[i]=[];
-		for(var j=0;j<this.column;j++)
+		for(var j=0;j<this.columns;j++)
 		{
 	//		this.cells[i][j]=new CELL(i,j,mapData_kind[i][j],mapData_who[i][j]);
 			this.cells[i][j]=new CELL(i,j,1,0);
@@ -60,9 +60,9 @@ FIELD.prototype.makeField=function()
  */
 FIELD.prototype.draw=function()
 {
-	for(var i=0;i<this.row;i++)
+	for(var i=0;i<this.rows;i++)
 	{
-		for(var j=0;j<this.column;j++)
+		for(var j=0;j<this.columns;j++)
 		{
 			this.cells[i][j].draw();
 		}
@@ -72,7 +72,7 @@ FIELD.prototype.draw=function()
  *
  * 각 셀마다 클릭 여부를 체크, 클릭한 셀의 인덱스 no와 셀의 유형을 리턴한다.
  *
- * @return {object}	메인 함수에 전달할 값들
+ * @return {COORD}	메인 함수에 전달할 값들
  			index:자신의 인덱스 no.
 			signal:버튼 종류(0:움직일 수 없는 셀, 1:움직일 수 있는 셀, 2:필러)
 			없을 시 null 반환
@@ -80,9 +80,9 @@ FIELD.prototype.draw=function()
  */
 FIELD.prototype.clickCheck=function()
 {
-	for(var i=0;i<this.row;i++)
+	for(var i=0;i<this.rows;i++)
 	{
-		for(var j=0;j<this.column;j++)
+		for(var j=0;j<this.columns;j++)
 		{
 			if(this.cells[i][j].isMouseOn())
 			{
