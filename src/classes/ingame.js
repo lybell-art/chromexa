2cf14835cb0f2c81cbeaf88575a7b2a4855719bb
steliviere <32293736+steliviere@users.noreply.github.com>
2017-12-16 23:47:24 +0900

    Update ingame.js

--- src/classes/ingame.js
+++ src/classes/ingame.js
@@ -131,8 +131,8 @@ INGAME.prototype.input=function()
 		}
 		cSel=this.charaSelect(clickSignal.index);	//캐릭터를 클릭했는지를 파악함
 		if(cSel!=-1) clickSignal.signal=_CHARA;
-		console.log(clickSignal, cSel);
-		if(this.currnentP!=-1)	//캐릭터 선택이 안 된 상태일 때
+		console.log(clickSignal, cSel, this.currentP);
+		if(this.currentP!=-1)	//캐릭터 선택이 안 된 상태일 때
 		{
 			if(clickSignal.signal==_CHARA) this.currentP=cSel;
 //			else if(clickSignal.signal==_FILLER) this.filler();
