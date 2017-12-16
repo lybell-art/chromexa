adf63f507d82be914e67b5d0a1a62d5635ddea18
steliviere <32293736+steliviere@users.noreply.github.com>
2017-12-16 18:25:33 +0900

    Update loading.js

--- src/classes/loading.js
+++ src/classes/loading.js
@@ -14,7 +14,7 @@ LOADING.prototype.loadData=function()
 		{
 			resourceBox.map[i][j]=new MAP_DATA();
 			url="resource/"+i+"-"+j+".csv";
-			table=loadTable(url, "csv", "header", this.inputMap(table,resourceBox.map[i][j]));
+			table=loadTable(url, "csv", "header", function(){this.inputMap(table,resourceBox.map[i][j])});
 			this.max++;
 		}
 	}
