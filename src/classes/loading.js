7c70e3b29a09d34c24d86982f193ef6877d5631d
steliviere <32293736+steliviere@users.noreply.github.com>
2017-12-16 18:15:38 +0900

    Update loading.js

--- src/classes/loading.js
+++ src/classes/loading.js
@@ -3,7 +3,7 @@ function LOADING()
 	this.count=0;
 	this.max=0;
 }
-LOADING.prototype.loadData()
+LOADING.prototype.loadData=function()
 {
 	var i,j;
 	var table, url;
@@ -14,20 +14,36 @@ LOADING.prototype.loadData()
 		{
 			resource_map[i][j]=new MAP_DATA();
 			url="resource/"+i+"-"+j+".csv";
-			table=loadTable(url, "csv", "header",
-				function(res){
-					resource_map[i][j].inputTable(res);
-					this.count++;
-			});
+			table=loadTable(url, "csv", "header", this.inputMap(table,resource_map[i][j]));
 			this.max++;
 		}
 	}
 }
-LOADING.prototype.loadImage()
+LOADING.prototype.execute=function()
 {
 	background(0);
 	fill(255);
 	noStroke();
 	rect(0,0,map(this.count,0,this.max,0,width),50);
 	if(this.count==this.max) sceneNo=10;
+}
+LOADING.prototype.inputMap=function(table, box)
+{
+	box.row=table.getRowCount();
+	box.column=table.getColumnCount();
+	var s="";
+	for(var i=0;i<box.row;i++)
+	{
+		box.kind[i]=[];
+		box.who[i]=[];
+		box.enemy[i]=[];
+		for(var j=0;j<box.column;j++)
+		{
+			s=split(table.getString(i,j),"|");
+			box.kind[i][j]=int(s[0]);
+			box.who[i][j]=int(s[1]);
+			box.enemy[i][j]=int(s[2]);
+		}
+	}
+	this.count++;
 }
