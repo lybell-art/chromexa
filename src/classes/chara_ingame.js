2d1cc4be135470f1ac6d6a52906abf9c25fa201d
steliviere <32293736+steliviere@users.noreply.github.com>
2017-12-17 01:39:51 +0900

    Update chara_ingame.js

--- src/classes/chara_ingame.js
+++ src/classes/chara_ingame.js
@@ -92,7 +92,7 @@ CHARACTER_INGAME.prototype.attack=function(map, otherPlayers, otherEnemys)
 	{
 		for(var j=0;j<11;j++)
 		{
-			console.log(cRow(i), cCol(j), cells[cRow(i)+j%2][cCol(j)]);
+			console.log(cRow(i), cCol(j), cells[cRow(i)+j%2][cCol(j)], cells);
 			if(cRow(i)+j%2<0||cRow(i)+j%2>=map.row||cCol(i)<0||cCol(i)>=map.col) continue;
 			console.log("dimi");
 /*			if(this.attackMap[i][j]&&cells[cRow(i)+j%2][cCol(j)].who!=-1)
