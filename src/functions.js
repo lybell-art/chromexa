82337b3662fd0fe3b67a3c78b683131f1d824a77
steliviere <32293736+steliviere@users.noreply.github.com>
2017-12-17 17:00:09 +0900

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -39,15 +39,3 @@ function detectCell(kind)
 		default:return _NOMOVE;
 	}
 }
-
-function clone(obj) {
-	console.log(obj);
-	if (obj === null || typeof(obj) !== 'object') return obj;
-	var copy = obj.constructor();
-	for (var attr in obj) {
-		if (obj.hasOwnProperty(attr)) {
-			copy[attr] = clone(obj[attr]);
-		}
-	}
-	return copy;
-}
