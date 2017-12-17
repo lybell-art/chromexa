eb716b84b45cd2405a7f447a0d323f1a5cacc597
steliviere <32293736+steliviere@users.noreply.github.com>
2017-12-17 16:17:31 +0900

    Update ingame.js

--- src/classes/ingame.js
+++ src/classes/ingame.js
@@ -46,6 +46,7 @@ INGAME.prototype.setup=function()
 	var mapData=resourceBox.map[this.world][this.stage].copy();
 	this.field=new FIELD();
 	this.field.makeField(mapData);
+	console.log(this.field);
 	//create players
 	for(var i=0;i<this.field.Rows;i++)
 	{
