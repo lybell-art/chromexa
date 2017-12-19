10381b11792073594a820b4a6000d67797d17bb1
steliviere <32293736+steliviere@users.noreply.github.com>
2017-12-19 17:05:42 +0900

    Update ingame.js

--- src/classes/ingame.js
+++ src/classes/ingame.js
@@ -172,7 +172,7 @@ INGAME.prototype.input=function()
 			case "swap":
 				if(this.whosTurn==1) thisChara=this.p1[this.currentP];
 				else thisChara=this.p2[this.currentP];
-				thisChara.move(this, this.otherPlayerDialog.p.coord);
+				thisChara.move(this, this.other_dialogButton.p.coord);
 				break;
 		}
 		this.otherPlayerDialog=-1;
