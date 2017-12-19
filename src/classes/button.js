15917ea3aaa03649118f66f6a688eac3f4477b8b
steliviere <32293736+steliviere@users.noreply.github.com>
2017-12-19 16:55:25 +0900

    Update button.js

--- src/classes/button.js
+++ src/classes/button.js
@@ -45,23 +45,24 @@ HEXA_BUTTON.prototype.mouseOn=function(isSC_on)
 
 function OTHER_DIALOGBUTTON()
 {
+	this.p=null
 	this.x=0;
 	this.y=0;
 	this.isValid=false;
 }
 OTHER_DIALOGBUTTON.prototype.set=function(where,otherP)
 {
-	var p;
 	if(otherP!=-1)
 	{
-		if(where.whosTurn==1) p=where.p1[otherP];
-		else p=where.p2[otherP];
+		if(where.whosTurn==1) this.p=where.p1[otherP];
+		else this.p=where.p2[otherP];
 		this.x=p.x;
 		this.y=p.y;
 		this.isValid=true;
 	}
 	else
 	{
+		this.p=null;
 		this.x=0;
 		this.y=0;
 		this.isValid=false;
